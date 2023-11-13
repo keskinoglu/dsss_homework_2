e423322ed2ee9855d741aee4d23a6755fde10d4b
keskinoglu <tolga.keskinoglu@fau.de>
2023-11-13 19:49:19 +0100

    Refactor math quiz code to improve readability and add unit tests

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,94 @@
 import random
 
 
-def function_A(min, max):
+def generate_random_number(min, max):
     """
-    Random integer.
+    Generate a random integer from a given range.
+
+    Args:
+        min (int): The lower bound of the range.
+        max (int): The upper bound of the range.
+
+    Returns:
+        int: A random integer within the specified range.
     """
     return random.randint(min, max)
 
 
-def function_B():
+def generate_random_arithmetic_operator():
+    """
+    Generate a random arithmetic operator from the following list: +, -, *.
+
+    Returns:
+        str: A random operator ('+', '-', or '*').
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def generate_math_problem(first_operand, second_operand, operator):
+    """
+    Generate a math problem and calculate the answer.
+
+    Args:
+        first_operand (int): The first operand in the problem.
+        second_operand (int): The second operand in the problem.
+        operator (str): The operator in the problem.
+
+    Returns:
+        tuple: A tuple containing the problem as a string and the answer as an integer.
+    """
+    problem = f"{first_operand} {operator} {second_operand}"
+
+    if operator == '+': answer = first_operand + second_operand
+    elif operator == '-': answer = first_operand - second_operand
+    else: answer = first_operand * second_operand
+
+    return problem, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Run a math quiz game.
+
+    The game generates a series of math problems and prompts the user for their answers.
+    It keeps track of the user's score and prints it at the end.
+
+    Returns:
+        None
+    """
+    score = 0
+    number_of_questions = 4
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(number_of_questions):
+        first_operand = generate_random_number(1, 10)
+        second_operand = generate_random_number(1, 5)
+        operator = generate_random_arithmetic_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem, answer = generate_math_problem(first_operand, second_operand, operator)
 
-        if useranswer == ANSWER:
+        print(f"\nQuestion: {problem}")
+
+        # Prompt the user for their answer.
+        user_answer = input("Your answer: ")
+
+        # Check if the user entered a valid integer.
+        while not user_answer.isdigit():
+            print("Please enter a valid integer.")
+            user_answer = input("Your answer: ")
+
+        # Convert the user's answer to an integer.
+        user_answer = int(user_answer)
+
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
-        else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+            score += 1
+        else:
+            print(f"Wrong answer. The correct answer is {answer}.")
+
+    print(f"\nGame over! Your score is: {score}/{number_of_questions}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,52 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_number
+from math_quiz import generate_random_arithmetic_operator
+from math_quiz import generate_math_problem
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
-        # Test if random numbers generated are within the specified range
+    def test_generate_random_number(self):
+        """
+        Test if random numbers generated are within the specified range.
+        """
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = generate_random_number(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_generate_random_arithmetic_operator(self):
+        """
+        Test if random arithmetic operators generated are valid.
+        """
+        for _ in range(1000):
+            rand_op = generate_random_arithmetic_operator()
+            self.assertTrue(rand_op in ['+', '-', '*'])
 
-    def test_function_C(self):
+    def test_generate_math_problem(self):
+            """
+            Test if math problems and answers generated are correct.
+            """
             test_cases = [
                 (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+                (5, 2, '-', '5 - 2', 3),
+                (5, 2, '*', '5 * 2', 10),
+                (-5, 2, '+', '-5 + 2', -3),
+                (-5, 2, '-', '-5 - 2', -7),
+                (-5, 2, '*', '-5 * 2', -10),
+                (5, -2, '+', '5 + -2', 3),
+                (5, -2, '-', '5 - -2', 7),
+                (5, -2, '*', '5 * -2', -10),
+                (-5, -2, '+', '-5 + -2', -7),
+                (-5, -2, '-', '-5 - -2', -3),
+                (-5, -2, '*', '-5 * -2', 10),
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            for first_operand, second_operand, operator, expected_problem, expected_answer in test_cases:
+                problem, answer = generate_math_problem(first_operand, second_operand, operator)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
 
 if __name__ == "__main__":
     unittest.main()
