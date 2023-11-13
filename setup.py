5da305d8eecc6b02d427d38b8da3dda16fa4170e
keskinoglu <tolga.keskinoglu@fau.de>
2023-11-13 21:03:01 +0100

    Added to setup.py file.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='0.1',
+    py_modules=['math_quiz'],
+    entry_points={
+        'console_scripts': [
+            'math_quiz = math_quiz:main',
+        ],
+    },
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
